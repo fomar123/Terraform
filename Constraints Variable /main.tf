da0a6d4c5e55fe50daa59af88113bc389606cbb9
fomar123 <90075757+fomar123@users.noreply.github.com>
2023-06-13 12:52:28 +0100

    Create main.tf

---
+++ Constraints Variable /main.tf
@@ -0,0 +1,30 @@
+provider "aws" {
+    region =  "us-east-1"
+    access_key = "AKIA5VOS5ZHSIQIAGJX6"
+    secret_key = "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"
+}
+
+variable "subnet_cidr_block" {
+    description = "subnet cidr block"
+}
+
+variable "cidr_blocks" {
+    description = "cidr blocks for vpc and subnets"
+    type = list(string)
+}   
+
+resource "aws_vpc" "development-vpc" {
+    cidr_block = var.cidr_blocks[0]
+    tags = {
+        Name = "development"
+    }
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+    vpc_id = aws_vpc.development-vpc.id
+    cidr_block = var.cidr_blocks[1]
+    availability_zone = "us-east-1a"
+        tags = {
+        Name = "subnet-1-dev"
+    }
+}
